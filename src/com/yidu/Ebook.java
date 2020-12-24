76f068a39cdcc226c731319e2b29e074888f7e8b
MengYiChang1011 <3287386207@qq.com>
2020-12-24 16:09:01 +0800

    第二次测试

---
+++ src/com/yidu/Ebook.java
@@ -0,0 +1,13 @@
+package com.yidu;
+
+/**
+ * @author zengshihao
+ * @version 1.0
+ * @description:
+ * @date 2020/12/24 16:05
+ */
+public class Ebook {
+    public static void main(String[] args) {
+        System.out.println("书籍测试");
+    }
+}
